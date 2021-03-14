e5c793f9504b11278cdab431126f4b01281e7718
AbrahamMichaelAntonius <122202002811@mhs.dinus.ac.id>
2021-03-15 03:33:27 +0700

    Update

---
+++ minggu 1/Komentar.php
@@ -0,0 +1,3 @@
+<?
+echo "Tarraaa Muncul";
+?>
---
+++ minggu 1/Operator.php
@@ -0,0 +1,7 @@
+<?
+$gaji = 1500000;
+$pajak = 0.3;
+$thp = $gaji - ($gaji*$pajak);
+echo "Upah Murni = Rp. $gaji <br>";
+echo "Upah setelah dikurangi pajak = Rp. $thp";
+?>
